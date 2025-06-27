8a6d97e5e1490c0140e64c5bfa8620efb87d5fa1
Ajogu Joseph <ajogujoseph0317@gmial.com>
2025-06-27 14:57:39 +0100

    fixed bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,5 +4,9 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/auth/login");
+  });
   return;
 }
